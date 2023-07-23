35d71876c2465d795ac5932e7ba82e0a43418df9
Faissal Moustafa <faissalmoustafa@gmail.com>
2023-07-24 01:11:41 +0300

    doing JS for picture menu

--- front-end/sources/script.js
+++ front-end/sources/script.js
@@ -20,6 +20,29 @@ function closeSidebar(event) {
     }
 }
 
+const pic_btn=document.getElementById("change_btn")
 
+const pic_modal=document.getElementById("change-pic-modal")
 
+const pic_menu=document.getElementById("modal_content")
 
+const close_btn=document.getElementById("close_btn")
+
+pic_btn.addEventListener("click",bringPicMenu)
+
+function bringPicMenu() {
+    pic_modal.style.display='flex';
+}
+
+close_btn.addEventListener("click",closePicMenu1)
+
+function closePicMenu1() {
+    pic_modal.style.display = 'none';
+}
+
+document.addEventListener('click', closePicMenu2);
+function closePicMenu2(event) {
+    if (!pic_menu.contains(event.target) && !pic_btn.contains(event.target)) {
+        pic_modal.style.display = 'none';
+    }
+}

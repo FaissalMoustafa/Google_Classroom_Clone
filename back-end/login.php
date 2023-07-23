33ae5f4ea533c6d304123a27d962f6597ef522bf
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-24 00:41:25 +0300

    [Back-end > login.php] Rearranged variables in bind_result()

--- back-end/login.php
+++ back-end/login.php
@@ -7,12 +7,12 @@ $_POST = json_decode($json_data, true);
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$query = $mysqli->prepare('select * from users where email=?');
+$query = $mysqli->prepare('select * from users where user_email=?');
 $query->bind_param('s', $email);
 $query->execute();
 $query->store_result();
 
-$query->bind_result($id,$firstname,$lastname,$email, $hashed_password,$type);
+$query->bind_result($id,$email,$hashed_password,$firstname,$lastname,$type);
 $query->fetch();
 
 $num_rows = $query->num_rows();
@@ -26,7 +26,7 @@ if ($num_rows == 0) {
         $response['firstname'] = $firstname;
         $response['lastname'] = $lastname;
         $response['email'] = $email;
-        $response['role'] = $type;
+        $response['role'] = $type; 
     } else {
         $response['status'] = "Failed";
         $response['message'] = "Incorrect Password";

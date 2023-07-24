edcf9b8ca2577967300f4fff39f7b6380f53512d
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-24 12:53:55 +0300

    [back-end > modules > get_user_classes.php] Declared new query stmt_class_memebers

--- back-end/modules/get_user_classes.php
+++ back-end/modules/get_user_classes.php
@@ -1,7 +1,7 @@
 <?php
     $root = "C:\\xampp\\htdocs\\Google_Classroom_Clone\\";
     include("$root.\\back-end\\connection.php");
-    
+
     function getClasses($mysqli, $user_id){
         $stmt_class_details = "
         select u.classroom_id, c.class_title, c.teacher_id 
@@ -9,7 +9,8 @@
         join classrooms as c on u.classroom_id = c.classroom_id
         where student_id = ?;
         ";
-
+        $stmt_class_members = "
+        ";
         $get_user_classrooms = $mysqli->prepare($stmt_class_details);
         $get_user_classrooms->bind_param('s',$user_id);
         $get_user_classrooms->execute();

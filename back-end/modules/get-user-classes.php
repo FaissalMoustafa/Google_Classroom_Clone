16f11885c5f42bafb0a8bd8168739b6bdeb2f939
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-24 17:32:09 +0300

    [back-end > modules > get_user_classes.php] Added class description to classes array

--- back-end/modules/get-user-classes.php
+++ back-end/modules/get-user-classes.php
@@ -4,7 +4,7 @@
 
     function getClasses($mysqli, $user_id){
         $stmt_class_details = "
-        select u.classroom_id, c.class_title, c.teacher_id 
+        select u.classroom_id, c.class_title, c.teacher_id, c.class_description 
         from classroom_users u
         join classrooms as c on u.classroom_id = c.classroom_id
         where student_id = ?;
@@ -15,7 +15,7 @@
         $get_user_classrooms->execute();
         $get_user_classrooms->store_result();
 
-        $get_user_classrooms->bind_result($class_id, $class_title, $teacher_id);
+        $get_user_classrooms->bind_result($class_id, $class_title, $teacher_id, $class_description);
 
         $classes = array();
         if($get_user_classrooms->num_rows() > 0){
@@ -23,6 +23,7 @@
                 $classes[] = [
                     "class_id"=>$class_id,
                     "title"=>$class_title,
+                    "description"=>$class_description,
                     "teacher_id"=>$teacher_id
                 ];
             }

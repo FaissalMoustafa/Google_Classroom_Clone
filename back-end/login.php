13e20d4579dace98d144224894a210e181f2a06d
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-24 17:02:50 +0300

    [back-end > login.php] Added list of classes to JSON response

--- back-end/login.php
+++ back-end/login.php
@@ -1,5 +1,7 @@
 <?php
-include('connection.php');
+include 'connection.php';
+$root = $_SERVER['DOCUMENT_ROOT']."\\Google_Classroom_Clone\\";
+include $root."back-end\\modules\\get-user-classes.php";
 
 $json_data = file_get_contents('php://input');
 $_POST = json_decode($json_data, true);
@@ -26,7 +28,8 @@ if ($num_rows == 0) {
         $response['firstname'] = $firstname;
         $response['lastname'] = $lastname;
         $response['email'] = $email;
-        $response['role'] = $type; 
+        $response['role'] = $type;
+        $response['classes'] = getClasses($mysqli, $id);
     } else {
         $response['status'] = "Failed";
         $response['message'] = "Incorrect Password";

f1674c60454efc91a4f260caa154f2beb923ef7d
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-25 01:53:49 +0300

    [back-end > modules > create-class.php] Added SQL statement

--- back-end/modules/create-class.php
+++ back-end/modules/create-class.php
@@ -12,6 +12,12 @@ include $root."back-end\\connection.php";
     }
 */
 
-function createClass($mysqli, $teacher_id){
-    
+function createClass($mysqli, $teacher_id, $class_title, $class_description){
+    //database query
+    $stmt_create_class = "
+        insert into classrooms
+        (teacher_id, class_title, class_description)
+        values
+        (?,?,?)
+    ";
 }

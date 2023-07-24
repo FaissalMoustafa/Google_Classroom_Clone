cf3a9f442c5fd3b0ce592a09b8f9c01316d104e8
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-24 17:06:31 +0300

    [back-end > modules > get_user_classes.php] Members list is no longer returned from this module

---
+++ back-end/modules/get-user-classes.php
@@ -0,0 +1,36 @@
+<?php
+    $root = $_SERVER['DOCUMENT_ROOT']."\\Google_Classroom_Clone\\";
+    include $root."back-end\\connection.php";
+
+    function getClasses($mysqli, $user_id){
+        $stmt_class_details = "
+        select u.classroom_id, c.class_title, c.teacher_id 
+        from classroom_users u
+        join classrooms as c on u.classroom_id = c.classroom_id
+        where student_id = ?;
+        ";
+
+        $get_user_classrooms = $mysqli->prepare($stmt_class_details);
+        $get_user_classrooms->bind_param('s',$user_id);
+        $get_user_classrooms->execute();
+        $get_user_classrooms->store_result();
+
+        $get_user_classrooms->bind_result($class_id, $class_title, $teacher_id);
+
+        $classes = array();
+        if($get_user_classrooms->num_rows() > 0){
+            while($get_user_classrooms->fetch()){
+                $classes[] = [
+                    "class_id"=>$class_id,
+                    "title"=>$class_title,
+                    "teacher_id"=>$teacher_id
+                ];
+            }
+
+            return $classes;
+        } else {
+            return [];
+        }
+
+    };
+?>

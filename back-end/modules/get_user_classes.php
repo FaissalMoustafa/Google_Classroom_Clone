ebde533441b8f1ebb4ecd505bb70f1660ca62d41
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-24 12:50:28 +0300

    [back-end > modules > get_user_classes.php] Changed some variable names

--- back-end/modules/get_user_classes.php
+++ back-end/modules/get_user_classes.php
@@ -3,14 +3,16 @@
     include("$root.\\back-end\\connection.php");
     $con = $mysqli;
     function getClasses($mysqli, $user_id){
-        $statement = "
+        $stmt_class_details = "
         select u.classroom_id, c.class_title, c.teacher_id 
         from classroom_users u
         join classrooms as c on u.classroom_id = c.classroom_id
         where student_id = ?;
         ";
-        $get_user_classrooms = $mysqli->prepare($statement);
+
+        $get_user_classrooms = $mysqli->prepare($stmt_class_details);
         $get_user_classrooms->bind_param('s',$user_id);
         $get_user_classrooms->execute();
         $get_user_classrooms->store_result();
+
     };

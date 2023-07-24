5decd7759de91d770f2db9afe98d735ac55306ea
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-24 12:02:34 +0300

    [back-end > modules > get_user_classes.php] Implemented SQL query to retrieveclass list from DB

--- back-end/modules/get_user_classes.php
+++ back-end/modules/get_user_classes.php
@@ -2,5 +2,14 @@
     include('connection.php');
     
     function getClasses($user_id){
-
+        $statement = "
+        select u.classroom_id, c.class_title, c.teacher_id 
+        from classroom_users u
+        join classrooms as c on u.classroom_id = c.classroom_id
+        where student_id = ?;
+        ";
+        $get_user_classrooms = $mysqli->prepare($statement);
+        $get_user_classrooms->bind_param('s',$user_id);
+        $get_user_classrooms->execute();
+        $get_user_classrooms->store_result();
     }

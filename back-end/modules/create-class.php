fa58de943e60b653fcf255805d97a8356de10a2b
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-25 02:17:14 +0300

    [back-end > modules > create-class.php] Fixed syntax error

--- back-end/modules/create-class.php
+++ back-end/modules/create-class.php
@@ -22,11 +22,11 @@ function createClass($mysqli, $teacher_id, $class_title, $class_description){
     ";
 
     $create_class = $mysqli->prepare($stmt_create_class);
-    $create_class->bind_param($teacher_id, $class_title, $class_description);
+    $create_class->bind_param('sss',$teacher_id, $class_title, $class_description);
     $is_class_created = $create_class->execute();
 
     if($is_class_created)
         return $is_class_created;
-        
+
     return false;
 }

6716f279c6b221b79cf2f393732a0272e8309de9
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-25 22:46:04 +0300

    [back-end > join.php] Implemented logic to receive input

--- back-end/join.php
+++ back-end/join.php
@@ -4,4 +4,6 @@ include 'connection.php';
 $root = $_SERVER['DOCUMENT_ROOT']."\\Google_Classroom_Clone\\";
 include $root."back-end\\modules\\join-class.php";
 
+$json_data = file_get_contents('php://input');
+$_POST = json_decode($json_data, true);
 

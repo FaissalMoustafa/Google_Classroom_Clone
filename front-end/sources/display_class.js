ec9c32a8f764c1f58c982016e630f662a123e912
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-07-26 00:26:26 +0300

    storing user data

--- front-end/sources/display_class.js
+++ front-end/sources/display_class.js
@@ -1,10 +1,17 @@
 
 console.log("hi i'm in the class")
+
+// retrieving user data from the local storage
 const user_data = localStorage.getItem("myData");
-console.log(user_data)
+
 if (user_data !== null) {
     const parsedData = JSON.parse(user_data);
     console.log(parsedData); 
+
 } else {
     console.log('Data not found in local storage.');
 }
+
+const displayClass = function (a, b) {
+  return a * b;
+};

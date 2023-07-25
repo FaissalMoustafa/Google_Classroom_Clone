4ae4a46febce718699e0052caa6a987717ced249
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-25 22:49:24 +0300

    [back-end > join.php] Completed!

--- back-end/join.php
+++ back-end/join.php
@@ -7,3 +7,9 @@ include $root."back-end\\modules\\join-class.php";
 $json_data = file_get_contents('php://input');
 $_POST = json_decode($json_data, true);
 
+$user_id = $_POST['user_id'];
+$class_id = $_POST['class_id'];
+
+$response['status'] = joinClass($mysqli, $class_id, $user_id);
+
+echo json_encode($response);

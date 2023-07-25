cff2319900d015ab56f6bae89e631e5f6d7eeebd
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-07-25 23:49:21 +0300

    creating display class js

---
+++ front-end/sources/display_class.js
@@ -0,0 +1,10 @@
+
+console.log("hi i'm in the class")
+const user_data = localStorage.getItem(response);
+console.log(user_data)
+if (user_data !== null) {
+    const parsedData = JSON.parse(user_data);
+    console.log(parsedData); 
+} else {
+    console.log('Data not found in local storage.');
+}

d86368720b0348b9e01a8798bb3cb277d3a0846a
Faissal Moustafa <faissalmoustafa@gmail.com>
2023-07-26 04:00:19 +0300

    linking edit_profile front and back

--- front-end/sources/edit_profile.js
+++ front-end/sources/edit_profile.js
@@ -1,61 +1,66 @@
-const pages = {}
+// const pages = {}
 
-pages.base_url = "http://localhost//Google_Classroom_Clone/back-end/";
+// pages.base_url = "http://localhost//Google_Classroom_Clone/back-end/";
 
-pages.print_message = (message) =>{
-    console.log(message);
-}
-
-pages.postAPI = async (api_url, api_data) => {
-    try{
-        return await fetch(api_url,{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(api_data)
-        })
-        .then(res =>{
-            return res.json()
-        } )
-        .then(api_data => {
-            console.log(api_data)
-            return api_data
-        })
-    
-    }catch(error){
-        pages.print_message("Error from Linking (POST) " + error)
-    }
-}
-
-// pages.edit_prof = (page) => {
-//     console.log("submit")
-//     const form = document.getElementById("sub_change")
-
-//     form.addEventListener('submit', event => {
-
-//         console.log("I am in submit")
-//         event.preventDefault()
-//         const first_name = document.getElementById("first_name")
-//         const last_name = document.getElementById("last_name")
-//         const new_pass = document.getElementById("new_pass")
-//         const conf_pass = document.getElementById("conf_pass")
-//         const user_id=window.localStorage.getItem("uid")
-        
-//         const edit_prof_url = pages.base_url + "edit-profile.php"
-//         const response = await pages.postAPI(edit_prof_url,data)
-        
-//         if (response.status === "success") {
-//             console.log(response.message)
-//             setTimeout(() => {window.location.href = 'templates/log_in.html';}, 1000)
-            
-//         }else{
-//             console.log(response.message)
-//         }
-//     }
-//     )
+// pages.print_message = (message) =>{
+//     console.log(message);
 // }
 
+// pages.postAPI = async (api_url, api_data) => {
+//     try{
+//         return await fetch(api_url,{
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(api_data)
+//         })
+//         .then(res =>{
+//             return res.json()
+//         } )
+//         .then(api_data => {
+//             console.log(api_data)
+//             return api_data
+//         })
+    
+//     }catch(error){
+//         pages.print_message("Error from Linking (POST) " + error)
+//     }
+// }
+
+
+const form = document.getElementById("sub_change")
+
+profile_details.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    const form_data = new FormData(profile_details);
+    const user_id=window.localStorage.getItem("uid")
+    const first_name = document.getElementById("first_name")
+    const last_name = document.getElementById("last_name")
+    const new_pass = document.getElementById("new_pass")
+    const conf_pass = document.getElementById("conf_pass")
+
+    if(new_pass.value()===conf_pass.value()){
+        form_data.append(user_id,last_name,first_name,new_pass);
+        const form_data_object = {};
+        form_data.forEach((value, key)=>{
+        form_data_object[key] = value;
+        });
+    }  
+    const from_data_json = JSON.stringify(form_data_object);
+    fetch("http://localhost/Google_Classroom_Clone/back-end/edit-profile.php",{
+    method: 'POST',
+    body: from_data_json
+    })
+    .then((response)=>{
+    return response.json();
+    })
+    .then((details)=>{
+    return details
+    })
+    .catch((error)=>{console.log(error)});
+    });
 
 const pic_btn=document.getElementById("change_btn")
 
